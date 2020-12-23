4345801a873d8122f2c912f429795d6245d393ba
mujeeb <khanmujeen687@gmail.com>
2020-12-23 17:12:57 +0530

    adding production url

--- lib/src/utils/my_urls.dart
+++ lib/src/utils/my_urls.dart
@@ -1,6 +1,6 @@
 class MyUrls {
 
   // static final String serverUrl = 'http://192.168.0.17:8081';
-  static final String serverUrl = 'http://192.168.31.139:8081';
+  static final String serverUrl = 'https://wively.herokuapp.com';
 
 }
